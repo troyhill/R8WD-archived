f53f250d7ddaac20806ef5791a32c66cd5c9b456
Troy Hill <hill.troy@gmail.com>
2023-06-30 07:58:40 -0600

    Add files via upload

---
+++ inst/extdata/script_hello_worldv3.R
@@ -0,0 +1,4 @@
+### pass variables defined in a function to a sourced script
+# source(system.file('extdata', 'script_hello_worldv3.R', package = 'R8WD'))
+# args1 <- 'org name'
+message('Here is a data report for ', args1)
