5bbcad0f65fdef951b9c9c1183da26d321a632de
Troy Hill <hill.troy@gmail.com>
2023-07-02 18:05:18 -0600

    Add files via upload

--- R/cv.R
+++ R/cv.R
@@ -1,4 +1,4 @@
-#' Calculate Coefficient of Variation
+#' Calculate Coefficient of Variation (as percent not decimal fraction, e.g., values fall on a scale of 0-100)
 #'
 #' @param x a vector of two or more values
 #'
@@ -9,7 +9,7 @@
 #' cv(c(10, 20))
 #' cv(c(10:30))
 cv <- function(x) {
-  val <- sd(x, na.rm = TRUE)/mean(x, na.rm = TRUE)
+  val <- sd(x, na.rm = TRUE)/mean(x, na.rm = TRUE) * 100
   if (length(na.omit(x)) < 2) {
     val <- NA # has no meaning for n = 1
   }
