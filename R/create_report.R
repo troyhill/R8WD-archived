1f2097270ea1de261ded919a790350fa0988e265
Troy Hill <hill.troy@gmail.com>
2023-07-10 11:32:25 -0600

    added message when org is invalid

--- R/create_report.R
+++ R/create_report.R
@@ -7,6 +7,7 @@
 #' @param endDate final date for data used in the report, in format `\%m-\%d-\%Y`
 #' @param parameters parameters to use in report. Must be entered as an index of acceptable parameters listed in params$params (e.g., `parameters = c(1:4,7)`)
 #' @param extFile name of report-generating script, located in the inst/extdata folder of the R8WD R package
+#' @param prompt_user if TRUE, user is prompted to use one of the organization codes in the `tribes` object provided with `R8WD`.
 #'
 #' @return Quarto markdown document
 #' @export
@@ -18,13 +19,19 @@ create_report <- function(org = 'TURTLEMT',
                           startDate = '01-01-2015',
                           endDate   = '12-31-2022',
                           parameters = c(1:length(params$params)),
-                            extFile = 'script_generateReport.qmd') {
+                            extFile = 'script_generateReport.qmd',
+                          prompt_user = TRUE) {
   targetFile <- system.file('extdata', extFile, package = 'R8WD')
   newFile    <- tempfile('wqp_report', fileext = '.qmd')
   token      <- '\'REPLACE_THIS_TEXT\'' # capture quotations
 
   ### if org is a vector, create an insertable string. This is benign where length == 1, so no ifelse statement
   org <- paste0("\'", paste0(gsub(x = toupper(org), pattern = "\'|\"", replacement = ''), collapse = '\',\''), "\'")
+  if (prompt_user) {
+    if (!org %in% tribes) {
+      message(org, ' not found in list of Tribal organizations. Recommended organization names:\n', paste0(tribes, collapse = '\n'))
+    }
+  }
   ### test for presence
   # grep(token, readLines(targetFile), value = TRUE) # remove ^ and $ to make this work
   newText    <- gsub(x = readLines(targetFile), pattern = token, replacement = org)
