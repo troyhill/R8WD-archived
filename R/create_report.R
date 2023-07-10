4cdcf94ad5ce6c319f89c1c1e09276acef7f3650
Troy Hill <hill.troy@gmail.com>
2023-07-10 13:19:20 -0600

    Error if prompt_user = TRUE & Tribe not recognized

--- R/create_report.R
+++ R/create_report.R
@@ -27,7 +27,7 @@ create_report <- function(org = 'TURTLEMT',
 
   if (prompt_user) {
     if (!any(grepl(pattern = paste0('^', toupper(org), '$'), x = tribes))) {
-      message(toupper(org), ' not found in list of Tribal organizations. Recommended organization names:\n', paste0(sort(tribes), collapse = '\n'))
+      stop(toupper(org), ' not found in list of Tribal organizations. Recommended organization names:\n', paste0(sort(tribes), collapse = '\n'))
     }
   }
 
