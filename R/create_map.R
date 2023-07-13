740cb93099068293576ed85d654a8e0fc5de4ee1
Troy Hill <hill.troy@gmail.com>
2023-07-13 12:24:33 -0600

    Add files via upload

--- R/create_map.R
+++ R/create_map.R
@@ -3,7 +3,7 @@
 #' @param .data full WQP data frame with site location data
 #'
 #' @return an interactive map of sample locations displayed as a Leaflet plot
-#' 
+#'
 #' @importFrom leaflet addLegend
 #' @importFrom leaflet colorBin
 #' @importFrom leaflet addProviderTiles
@@ -13,31 +13,35 @@
 #' @importFrom leaflet addCircleMarkers
 #' @importFrom dplyr group_by
 #' @importFrom dplyr summarise
-#' 
-#' @example 
-#' data <- getWQP(organization = "TURTLEMT", startDate = "01-01-2015", endDate = "12-31-2022)
+#'
+#' @examples {
+#' \dontrun{
+#' data <- getWQP(organization = \"TURTLEMT\", startDate = \"01-01-2015\",
+#' endDate = \"12-31-2022\")
 #' create_map(data)
-#' 
+#' }
+#' }
+#'
 #' @export
 
 create_map <- function(.data){
   suppressWarnings({
-    
+
     # taken from this stackoverflow: https://stackoverflow.com/questions/58505589/circles-in-legend-for-leaflet-map-with-addcirclemarkers-in-r-without-shiny
     addLegendCustom <- function(map, colors, labels, sizes, opacity = 0.5){
       colorAdditions <- paste0(colors, "; border-radius: 50%; width:", sizes, "px; height:", sizes, "px")
-      labelAdditions <- paste0("<div style='display: inline-block;height: ", sizes, 
+      labelAdditions <- paste0("<div style='display: inline-block;height: ", sizes,
                                "px;margin-top: 4px;line-height: ", sizes, "px;'>", labels, "</div>")
-      
-      return(leaflet::addLegend(map, colors = colorAdditions, labels = labelAdditions, 
+
+      return(leaflet::addLegend(map, colors = colorAdditions, labels = labelAdditions,
                                 opacity = opacity, title = "Measurements"))
     }
-    
+
     site_size = data.frame(Sample_n = c("<9",">10",">50",">100",">200",">500",">1500"),
                            Point_size = c(3,5,8,10,15,20,30))
-    
-    sumdat = .data %>% dplyr::group_by(MonitoringLocationIdentifier, MonitoringLocationName, LatitudeMeasure, LongitudeMeasure) %>% 
-      dplyr::summarise("Sample_Count" = length(unique(ActivityIdentifier)), "Visit_Count" = length(unique(ActivityStartDate)), 
+
+    sumdat = .data %>% dplyr::group_by(MonitoringLocationIdentifier, MonitoringLocationName, LatitudeMeasure, LongitudeMeasure) %>%
+      dplyr::summarise("Sample_Count" = length(unique(ActivityIdentifier)), "Visit_Count" = length(unique(ActivityStartDate)),
                        "Parameter_Count" = length(unique(CharacteristicName)), "Organization_Count" = length(unique(OrganizationIdentifier)))
     sumdat$radius = 3
     sumdat$radius = ifelse(sumdat$Sample_Count>10,5,sumdat$radius)
@@ -46,23 +50,23 @@ create_map <- function(.data){
     sumdat$radius = ifelse(sumdat$Sample_Count>200,15,sumdat$radius)
     sumdat$radius = ifelse(sumdat$Sample_Count>500,20,sumdat$radius)
     sumdat$radius = ifelse(sumdat$Sample_Count>1500,30,sumdat$radius)
-    
+
     site_legend = subset(site_size, site_size$Point_size %in% unique(sumdat$radius))
-    
+
     pal <- leaflet::colorBin(
       palette = "Blues",
       domain = sumdat$Parameter_Count)
-    
+
     map = leaflet::leaflet()%>%
-      leaflet::addProviderTiles("Esri.WorldTopoMap", group = "World topo", 
+      leaflet::addProviderTiles("Esri.WorldTopoMap", group = "World topo",
                                 options = leaflet::providerTileOptions(updateWhenZooming = FALSE,updateWhenIdle = TRUE)) %>%
       # get rid of whatever was there before if loading a second dataset
       leaflet::clearShapes() %>%
       # fit boundaries of the map to a relevant window
-      leaflet::fitBounds(lng1 = min(sumdat$LongitudeMeasure), lat1 = min(sumdat$LatitudeMeasure), 
-                         lng2 = max(sumdat$LongitudeMeasure), lat2 = max(sumdat$LatitudeMeasure)) %>% 
+      leaflet::fitBounds(lng1 = min(sumdat$LongitudeMeasure), lat1 = min(sumdat$LatitudeMeasure),
+                         lng2 = max(sumdat$LongitudeMeasure), lat2 = max(sumdat$LatitudeMeasure)) %>%
       leaflet::addCircleMarkers(data = sumdat, lng =~ as.numeric(LongitudeMeasure), lat =~ as.numeric(LatitudeMeasure),
-                                color="black",fillColor =~ pal(Parameter_Count), fillOpacity = 0.7, 
+                                color="black",fillColor =~ pal(Parameter_Count), fillOpacity = 0.7,
                                 stroke = TRUE, weight = 1.5, radius=sumdat$radius,
                                 popup = paste0("Site ID: ", sumdat$MonitoringLocationIdentifier,
                                                "<br> Site Name: ", sumdat$MonitoringLocationName,
@@ -71,7 +75,7 @@ create_map <- function(.data){
                                                "<br> Characteristic Count: ", sumdat$Parameter_Count)) %>%
       leaflet::addLegend("bottomright", pal = pal, values =sumdat$Parameter_Count,
                          title = "Characteristics", opacity = 0.5) %>%
-      addLegendCustom(colors = "black", 
+      addLegendCustom(colors = "black",
                       labels = site_legend$Sample_n, sizes = site_legend$Point_size*2)
     return(map)
   })
--- R/joinWQPProfiles.R
+++ R/joinWQPProfiles.R
@@ -1,43 +1,36 @@
 #' Join WQP Measurement and Site Data
 #'
 #' @description This function takes data from the same WQP query and combines
-#'              site data with measurements to map observations with physical 
+#'              site data with measurements to map observations with physical
 #'              sampling locations
 #'
-#' @param dat.full output of query to WQP portal using readWQPdata function in 
+#' @param fullData output of query to WQP portal using readWQPdata function in
 #'        the dataRetrieval package
-#' @param dat.sites output of query to WQP portal using whatWQPdat.sites function in the
+#' @param sites output of query to WQP portal using whatWQPsites function in the
 #'        dataRetrieval package
-#'        
+#'
 #' @return a combined data frame of WQP measurements with corresponding sampling
 #'         site data
-#'
-#' @importFrom dplyr left_join
-#' @importFrom dplyr rename_at
-#' @importFrom dplyr select_at
-#' @importFrom dplyr select
-#' @importFrom dplyr vars
-#' @importFrom dplyr ends_with
-#' @importFrom stringr str_replace
 #' @export
 
-joinWQPProfiles <- function(full = "null",
+joinWQPProfiles <- function(fullData = "null",
                             sites = "null"){
-  
-  dat.full <- full
-  dat.sites <- sites
-  
+
+  fullData.df <- fullData
+
+  sites.df <- sites
+
   # Join station data to the WQP data
-  if (length(dat.sites > 1)) {
-    if (nrow(dat.sites) > 0) {
-      join1 <- dat.full %>%
+  if (length(sites.df > 1)) {
+    if (nrow(sites.df) > 0) {
+      join1 <- fullData.df %>%
         # join stations to results
-        dplyr::left_join(dat.sites, by = "MonitoringLocationIdentifier", multiple = "all") %>%
+        dplyr::left_join(sites.df, by = "MonitoringLocationIdentifier", multiple = "all") %>%
         # remove ".x" suffix from column names
         dplyr::rename_at(dplyr::vars(dplyr::ends_with(".x")), ~ stringr::str_replace(., "\\..$", "")) %>%
         # remove columns with ".y" suffix
         dplyr::select_at(dplyr::vars(-dplyr::ends_with(".y")))
-    } else {join1 = dat.full}
-  } else {join1 = dat.full}
-  return(join1)
+    } else {join1 = fullData.df}
+  } else {join1 = fullData.df}
+  return (join1)
 }
